bb1aee126880ec38b9aec753e91c34109143064a
SirichaiEiEi <108708170+SirichaiEiEi@users.noreply.github.com>
2022-07-07 16:26:56 +0700

    Game5

--- Assets/GamePlay1.cs
+++ Assets/GamePlay1.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 using TMPro;
 using System;
 
-public class GamePlay : MonoBehaviour
+public class GamePlay1 : MonoBehaviour
 {
     public TMP_Text Text1;
     public TMP_Text Text2;
     private int counter;
+    private int countera;
     void Start()
     {
-         counter = 0;
-    }
+        counter = 0;
+        countera = 0;
+}
     void Update()
     {
         Text1.text = "Score:" + counter;
-        if (Text1.text == 120)
+        if (countera == 30)
             Text2.text = "You WIN";
         if (Input.GetMouseButtonDown(0))
         {
@@ -25,17 +27,20 @@ public class GamePlay : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
-                counter = counter - 20;
+                    counter = counter - 20;
+                    countera ++;
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
                 if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
-                counter = counter + 10;
+                    counter = counter + 10;
+                    countera++;
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
                 if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
-                counter = counter + 20;
+                    counter = counter + 20;
+                    countera++;
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
